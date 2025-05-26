4bd159afcca3460441afdaf3f4b65c2997748ccf
AhmedKhaled858 <tetaahmed858@gmail.com>
2025-05-26 13:58:51 +0300

    create home_remote_data_source

---
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -0,0 +1,20 @@
+import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeaturedBook();
+  Future<List<BookEntity>> fetchNewestBook();
+}
+
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  @override
+  Future<List<BookEntity>> fetchFeaturedBook() {
+    // TODO: implement fetchFeaturedBook
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<BookEntity>> fetchNewestBook() {
+    // TODO: implement fetchNewestBook
+    throw UnimplementedError();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -22,3 +22,4 @@ class BooklyApp extends StatelessWidget {
     );
   }
 }
+
