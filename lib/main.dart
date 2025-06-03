33bdcc64ea184ee3de370bed6c39cd670366d1e1
AhmedKhaled858 <tetaahmed858@gmail.com>
2025-06-03 21:22:38 +0300

    create bloc provider for featured and newest books

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,56 @@
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/features/home/domain/usecase/fetch_featured_book_use_case.dart';
+import 'package:bookly_app/features/home/presentation/manager/featured_book_cubit/featured_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
+import 'core/utils/functions/setup_service_locator.dart';
 
 void main() async {
-  runApp(const BooklyApp());
+  setUpServiceLocator();
+    await Hive.initFlutter();
     Hive.registerAdapter(BookEntityAdapter());
-   await Hive.openBox(kFeaturedBox);
+    await Hive.openBox<BookEntity>(kFeaturedBox);
+    await Hive.openBox<BookEntity>(kNewestBox);
+  runApp(const BooklyApp());
 }
 
+final getIt = GetIt.instance;
 class BooklyApp extends StatelessWidget {
   const BooklyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) {
+          return FeaturedBooksCubit(
+            FetchFeaturedBookUseCase(
+              getIt.get<HomeRepoImpl>(),
+            ),
+          );
+        },),
+        BlocProvider(create: (context) {
+          return FeaturedBooksCubit(
+            FetchFeaturedBookUseCase(
+              getIt.get<HomeRepoImpl>(),
+            ),
+          );
+        }),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
     );
   }
